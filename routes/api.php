d82e1145a11056542c49d98192b8ed353f9e1098
ThallesBogar <thallesbogar@gmail.com>
2024-01-27 23:07:35 -0300

    feat: Add new routes to api.php
    - Add route for listing all products
    - Add routes for sales

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('products')->group(function(){
+    Route::get('/list', [ProductController::class, 'list']);
+});
+
+Route::prefix('sales')->group(function(){
+    Route::post('/', 'SaleController@create');
+    Route::get('/list', 'SaleController@list');
+    Route::get('/{id}', 'SaleController@read');
+    Route::post('/{id}/cancel', 'SaleController@cancel');
+    Route::post('/{id}/products', 'SaleController@addProducts');
 });
