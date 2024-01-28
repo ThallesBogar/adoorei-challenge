681a8c51947ef0743288d59716c8d47df38a4e13
ThallesBogar <thallesbogar@gmail.com>
2024-01-27 23:16:56 -0300

    feat: Implement products listing
        - Add ProductController with list() method
        - Add new Query `GetAllProductsQuery` for listing products
    
        This feature helps to keep database read operations encapsulated followign CQRS principle.

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Queries\Product\GetAllProductsQuery;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function list()
+    {
+        try{
+            $products = GetAllProductsQuery::execute();
+
+            return response()->success('Products retrieved successfully', 200, $products);
+        }catch (\Exception $e){
+            return response()->error();
+        }
+    }
+}
---
+++ app/Queries/Product/GetAllProductsQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Queries\Product;
+
+use Illuminate\Support\Facades\DB;
+
+class GetAllProductsQuery
+{
+    public static function execute() : array
+    {
+        $query = "SELECT
+                      s.id as store_id,
+                      s.name as store_name,
+                      p.id as product_id,
+                      p.name as product_name,
+                      p.price as product_price,
+                      p.description as product_description
+                  FROM products p
+                      INNER JOIN stores s ON s.id = p.store_id
+                      INNER JOIN list_products_categories lpc ON lpc.id = p.list_product_category_id";
+
+        return DB::select($query);
+    }
+}
