57bf293ac1f86eafcfa99b1fbcd3049faaf88e22
ThallesBogar <thallesbogar@gmail.com>
2024-01-28 11:57:27 -0300

    test: Add integration tests for new endpoint: GET `/sales/{id}` and a test to check sale total_price
    - Add test to check if total_price is beeing calculated correctly when creating sales
    - Add test to check response status and Json structure when retrieving sale successfully
    - Add test to expect a 422 error when retrieving a non-existing sale
    - Add test to expect a 422 error when retrieving a sale with invalid id

--- tests/Feature/SaleTest.php
+++ tests/Feature/SaleTest.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Models\ListSaleStatus;
 use App\Models\Product;
+use App\Models\Sale;
 use Illuminate\Support\Facades\Artisan;
 
+use function Pest\Laravel\get;
+use function Pest\Laravel\getJson;
 use function Pest\Laravel\postJson;
 
 beforeEach(function (){
@@ -19,21 +23,63 @@ it('tests creation of valid sale', function () {
     Artisan::call('migrate:fresh');
     Artisan::call('db:seed');
 
-    $product = Product::first();
+    $product1 = Product::first();
+    $product2 = Product::skip(1)->first();
     $response = postJson('/api/sales', [
         'products' => [
             [
-                'id'     => $product->id,
-                'amount' => 1,
+                'id'     => $product1->id,
+                'amount' => 3,
             ],
             [
-                'id'     => $product->id,
+                'id'     => $product2->id,
                 'amount' => 2,
             ],
         ],
     ]);
 
     $response->assertStatus(200);
+    $response->assertJsonStructure([
+        'message',
+        'description',
+        'data' => [
+            'sale_id',
+            'currency',
+            'total_price',
+            'products' => [
+                '*' => [
+                    'product_id',
+                    'name',
+                    'price',
+                    'amount',
+                ]
+            ]
+        ]
+    ]);
+});
+
+it('tests if sale total_price is being calculated correctly when creating new sale', function () {
+    Artisan::call('migrate:fresh');
+    Artisan::call('db:seed');
+
+    $product1 = Product::first();
+    $product2 = Product::skip(1)->first();
+    $response = postJson('/api/sales', [
+        'products' => [
+            [
+                'id'     => $product1->id,
+                'amount' => 3,
+            ],
+            [
+                'id'     => $product2->id,
+                'amount' => 2,
+            ],
+        ],
+    ]);
+
+    $saleId = $response->json('data.sale_id');
+    $sale = Sale::find($saleId);
+    expect($sale->total_price)->toBe(3 * $product1->price + 2 * $product2->price);
 });
 
 it('tests creation of invalid sales', function (){
@@ -63,4 +109,62 @@ it('tests creation of invalid sales', function (){
         ]
     ]);
     $productWithoutAmount->assertStatus(422);
+});
+
+it('retrieves a sale successfully', function (){
+    Artisan::call('migrate:fresh');
+    Artisan::call('db:seed');
+
+    $product = Product::first();
+    $response = postJson('/api/sales', [
+        'products' => [
+            [
+                'id'     => $product->id,
+                'amount' => 1,
+            ],
+            [
+                'id'     => $product->id,
+                'amount' => 2,
+            ],
+        ],
+    ]);
+
+    $saleId = $response->json('data.sale_id');
+    $response = getJson("/api/sales/{$saleId}");
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        'message',
+        'description',
+        'data' => [
+            'sale_id',
+            'currency',
+            'total_price',
+            'products' => [
+                '*' => [
+                    'product_id',
+                    'name',
+                    'price',
+                    'amount',
+                ]
+            ]
+        ]
+    ]);
+});
+
+it('returns an error for retrieving a non-existent sale', function () {
+    Artisan::call('migrate:fresh');
+    $nonExistentId = 999999;
+
+    $response = getJson("/api/sales/{$nonExistentId}");
+
+    $response->assertStatus(422);
+    $response->assertJson(['message' => 'error']);
+});
+
+it('validates the input for retrieving a sale with invalid id', function () {
+    Artisan::call('migrate:fresh');
+    $response = getJson("/api/sales/not-an-id");
+
+    $response->assertStatus(422);
+    $response->assertJson(['message' => 'error']);
 });
